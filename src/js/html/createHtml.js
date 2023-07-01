adcf4cd37a7fe9a27c3d1da15d3f04ba4b65e039
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-01 14:34:59 +0200

    updated the createHtml component with more options

--- src/js/html/createHtml.js
+++ src/js/html/createHtml.js
@@ -1,19 +1,23 @@
 
 /**
- * 
+ * Create a html element component
  * @param {string} elementProp the element you want to create
- * @param {string} contentProp Content you want inside your element
  * @param {string} classProp class you want to set to you elements
- * @param {htmlNode} htmlParent HtmlNode you want to append the html element to
+ * @param {string} contentProp Content you want inside your element
+ * @param {htmlNode} htmlParent HtmlNode you want to append the html element to a container
+ * @param {string} data Use this to set a data attribute to your html element
+ * @param {string} dataContent sets the content for the data attribute
  * @return HTML element.
  * @example
  * ```js
- * createHtmlElement("div", "myClass", "content", HtmlElement)
+ * createHtmlElement("div", "myClass", "content", HtmlElement, "data-status", "show")
  * ```
  */
-export function createHtmlElement(elementProp, contentProp, classProp, htmlParent) {
+export function createHtmlElement(elementProp, classProp, contentProp, htmlParent, data, dataContent) {
+
   var htmlElement = document.createElement(`${elementProp}`);
   htmlElement.innerHTML = contentProp;
+  htmlElement.setAttribute(data, dataContent);
   htmlElement.classList.add(`${classProp}`);
-  htmlParent.appendChild(htmlElement);
+  htmlParent.append(htmlElement);
 }
