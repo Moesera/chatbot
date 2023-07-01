4daafd1e5d88d7e2c19e290b3a5fb525e1476cb8
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-01 15:31:53 +0200

    comments for later

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import { checkPlaceholderEmpty } from "./components/userInputs";
 
+//  move into a constants file / folder
 const userPrompt = document.getElementById("prompt");
 
+//  maybe move this into its own function and import it to keep it cleaner
 userPrompt.addEventListener('input', () => {
   userPrompt.style.height = 'auto';
   userPrompt.style.height = `${userPrompt.scrollHeight}px`;
@@ -15,3 +17,4 @@ userPrompt.addEventListener('input', () => {
 
   checkPlaceholderEmpty(userPrompt, "Message", "data-ph");
 });
+
