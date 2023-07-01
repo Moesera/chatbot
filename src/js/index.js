216eab1358b0ce6789cdb42c25e52c64aef09658
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-01 12:47:31 +0200

    resizes contenteditable to view text better

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const userPrompt = document.getElementById("prompt");
+
+userPrompt.addEventListener('input', () => {
+  userPrompt.style.height = 'auto';
+  userPrompt.style.height = `${userPrompt.scrollHeight}px`;
+
+  if (userPrompt.scrollHeight > 200) {
+    userPrompt.style.overflowY = 'scroll';
+    userPrompt.style.height = '100rem';
+  } else {
+    userPrompt.style.overflowY = 'hidden';
+  }
+
+  checkPlaceholderEmpty(userPrompt);
+});
