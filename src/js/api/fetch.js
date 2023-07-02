4a7fb0d72fef425dd3212a8dc95d98e20f4dfa99
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-02 05:26:09 +0200

    openAi fetch request

--- src/js/api/fetch.js
+++ src/js/api/fetch.js
@@ -1,17 +1,55 @@
+import { createHtmlElement } from "../html/createHtml";
 import { headers } from "./headers";
-import { apiPath } from "../constants/urls"
+import { apiPath } from "../constants/urls";
+import { loaderContainer } from "..";
 
 export async function fetchData(body, method) {
+  try {
+    const container = createHtmlElement("div", "bot-response", "", botResponseContainer);
+    loaderContainer.style.display = "flex";
 
-const res = await fetch(`${apiPath}`, {
-method: method,
-body: JSON.stringify(body),
-headers: headers("application/json")
-});
+    const res = await fetch(`${apiPath}`, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: headers("application/json"),
+    });
 
-if (Response.ok) {
-  return await res.json();
-}
+    if (!res.ok) {
+      const error = await res.json();
+      throw new Error(error.error.message);
+    }
 
-throw new Error(response.status);
+    const reader = res.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+
+    while (true) {
+      const chunk = await reader.read();
+      const { done, value } = chunk;
+
+      if (done) {
+        break;
+      }
+
+      const decodedChunk = decoder.decode(value);
+      const lines = decodedChunk.split("\n");
+      const parsedLines = lines
+        .map((line) => line.replace(/^data: /, "").trim())
+        .filter((line) => line !== "" && line !== "[DONE]")
+        .map((line) => JSON.parse(line));
+
+      for (const parsedLine of parsedLines) {
+        const { choices } = parsedLine;
+        const { delta } = choices[0];
+        const { content } = delta;
+
+        if (content) {
+          container.textContent += content;
+        }
+      }
+    }
+  } catch (error) {
+    return await error;
+  } finally {
+    loaderContainer.style.display = "none";
+  }
 }
