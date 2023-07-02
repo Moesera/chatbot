da9fd19ebc7e081e84bc9bee19661b99af3d29db
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-02 05:40:41 +0200

    html variables to own file

---
+++ src/js/constants/htmlVariables.js
@@ -0,0 +1,7 @@
+export const loaderContainer = document.querySelector('.loader-container');
+export const botResponseContainer = document.getElementById("botResponseContainer")
+export const userPrompt = document.getElementById("prompt");
+export const userInputForm = document.getElementById("userInputForm");
+export const submitButton = document.getElementById("submitButton");
+export const accessKeyInput = document.getElementById("accessKeyInput");
+export const accessKeyDelete = document.getElementById("accessKeyDelete");
